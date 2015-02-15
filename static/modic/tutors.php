64df095383790789baa827b29590f658d5f9debd
fishskinlau <fishskin@gmail.com>
2015-02-15 12:49:24 +0800

    Modic Static - fix tutor menu fonts
    used link tag from artist.php

--- static/modic/tutors.php
+++ static/modic/tutors.php
@@ -5,7 +5,10 @@
   <meta name="viewport" content="initial-scale=1.0">
     <meta name="viewport" content="width=980px,min-scale=1.0">
   <title>DANCE KINGDOM</title>
+  <!--
   <link href="http://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet" type="text/css">
+  -->
+  <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,400,300" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="css/standardize.css">
   <link rel="stylesheet" href="css/tutors-grid.css">
   <link rel="stylesheet" href="css/tutors.css">
